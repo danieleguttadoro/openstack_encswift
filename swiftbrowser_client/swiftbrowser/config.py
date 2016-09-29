413fe1eee52b9f4a283c5bb6cb9d6c9ae4fef39a
Daniele <daniele@ufitsandisk.lan>
2016-09-29 16:25:46 +0200

    Cleaning up

---
+++ swiftbrowser_client/swiftbrowser/config.py
@@ -0,0 +1,16 @@
+AUTH_URL   = 'http://127.0.0.1:8801'
+STORAGE_URL = 'http://127.0.0.1:8801'
+#META_TENANT = 'meta_encswift'
+
+SWIFT_USER = 'swift'
+
+PATH = 'swiftbrowser'
+
+
+#True: Over-Encryption set
+#Anyother value = Over-Encryption not used
+OVER_ENCRYPTION = True
+
+
+
+
